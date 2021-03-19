87bbda59b79fdf973378dbc9a62af5907a1f55ea
Rajesh Madamanchi <rmadamanchi@gmail.com>
2021-03-19 12:39:29 -0500

    improve matching logic to be case insensitive

--- input/input.go
+++ input/input.go
@@ -35,11 +35,14 @@ func (i *Input) Ask() {
 		_ = keyboard.Close()
 	}()
 
-	printSelection(i.Prompt, "", 0, i.Choices, 0)
 	input := ""
 	index := 0
-	choiceIndex := 0
+	selectedIndex := 0
 	matchingChoices := match(i.Choices, input)
+	viewStartIndex := 0
+	viewEndIndex := min(len(matchingChoices), 10)
+
+	printSelection(i.Prompt, input, index, matchingChoices, selectedIndex, viewStartIndex, viewEndIndex)
 
 	for {
 		char, key, err := keyboard.GetKey()
@@ -54,58 +57,82 @@ func (i *Input) Ask() {
 			input = input[:index] + string(char) + input[index:]
 			index += 1
 			matchingChoices = match(i.Choices, input)
+			viewStartIndex = 0
+			viewEndIndex = min(len(matchingChoices), 10)
+			selectedIndex = 0
 		} else if key == keyboard.KeySpace {
 			input = input[:index] + string(" ") + input[index:]
 			index += 1
 			matchingChoices = match(i.Choices, input)
+			viewStartIndex = 0
+			viewEndIndex = min(len(matchingChoices), 10)
+			selectedIndex = 0
 		} else if key == keyboard.KeyArrowLeft {
 			index = max(len(i.Prompt), index-1)
 		} else if key == keyboard.KeyArrowRight {
 			index = min(len(input), index+1)
 		} else if key == keyboard.KeyArrowDown {
-			choiceIndex = min(len(matchingChoices)-1, choiceIndex+1)
+			selectedIndex = min(len(matchingChoices)-1, selectedIndex+1)
+			if selectedIndex >= viewEndIndex {
+				viewStartIndex++
+				viewEndIndex++
+			}
 		} else if key == keyboard.KeyArrowUp {
-			choiceIndex = max(0, choiceIndex-1)
+			selectedIndex = max(0, selectedIndex-1)
+			if selectedIndex < viewStartIndex {
+				viewStartIndex--
+				viewEndIndex--
+			}
 		} else if key == keyboard.KeyBackspace {
 			if index > 0 {
 				input = input[:index-1] + input[index:]
 				index -= 1
 				matchingChoices = match(i.Choices, input)
+				viewStartIndex = 0
+				viewEndIndex = min(len(matchingChoices), 10)
+				selectedIndex = 0
 			}
 		} else if key == keyboard.KeyEnter && i.SelectedFn != nil {
-			i.SelectedFn(i.Choices[choiceIndex])
+			i.SelectedFn(matchingChoices[selectedIndex])
 		}
 
-		printSelection(i.Prompt, input, index, matchingChoices, choiceIndex)
+		printSelection(i.Prompt, input, index, matchingChoices, selectedIndex, viewStartIndex, viewEndIndex)
 	}
 }
 
 func match(choices []string, input string) []string {
 	matchingChoices := make([]string, 0)
 	for _, choice := range choices {
-		if strings.Contains(choice, input) {
+		if strings.Contains(strings.ToLower(choice), strings.ToLower(input)) {
 			matchingChoices = append(matchingChoices, choice)
 		}
 	}
 	return matchingChoices
 }
 
-func printSelection(prompt string, input string, index int, matchingChoices []string, choiceIndex int) {
-	fmt.Print("\x1b[s")       // save cursor
-	fmt.Print("\x1b[1000D")   // move cursor to left
-	fmt.Print("\x1b[K")       // clear line
-	fmt.Print(prompt + input) // print input
+func printSelection(prompt string, input string, index int, matchingChoices []string, selectedIndex int, viewStartIndex int, viewEndIndex int) {
+	fmt.Print("\x1b[s")                                  // save cursor
+	fmt.Print("\x1b[1000D")                              // move cursor to left
+	fmt.Print("\x1b[K")                                  // clear line
+	fmt.Print("\x1b[1;34m" + prompt + "\x1b[0m" + input) // print input
 	fmt.Println()
-	for i, choice := range matchingChoices {
-		fmt.Print("\x1b[K") // clear current line
-		matchIndex := strings.Index(choice, input)
+	for i := 0; i < len(matchingChoices); i++ {
+		choice := matchingChoices[i]
+		if i >= viewStartIndex && i < viewEndIndex {
+			fmt.Print("\x1b[K") // clear current line
+			matchIndex := strings.Index(strings.ToLower(choice), strings.ToLower(input))
 
-		if i == choiceIndex {
-			fmt.Println("\x1b[30;47m" + choice[0:matchIndex] + "\x1b[36m" + input + "\x1b[30;47m" + choice[matchIndex+len(input):] + "\x1b[0m")
-		} else {
-			fmt.Println(choice[0:matchIndex] + "\x1b[36m" + input + "\x1b[0m" + choice[matchIndex+len(input):])
+			preMatchPart := choice[0:matchIndex]
+			matchPart := choice[matchIndex : matchIndex+len(input)]
+			postMatchPart := choice[matchIndex+len(input):]
+			if i == selectedIndex {
+				fmt.Println("\x1b[30;47m" + preMatchPart + "\x1b[36m" + matchPart + "\x1b[30;47m" + postMatchPart + "\x1b[0m")
+			} else {
+				fmt.Println(preMatchPart + "\x1b[36m" + matchPart + "\x1b[0m" + postMatchPart)
+			}
 		}
 	}
+
 	fmt.Print("\x1b[0J")                                       // clear till end of screen
 	fmt.Print("\x1b[u")                                        // restore cursor
 	fmt.Print("\x1b[1000D")                                    // move cursor back to left
